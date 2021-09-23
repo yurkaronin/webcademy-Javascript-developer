af6295ad712bab1e39e87f618d6f684f57f0cbd7
GedeonWest <vladek-nebo@mail.ru>
2021-09-23 17:55:52 +0300

    Арифметические операции с простыми типами данных

---
+++ js/main-7.js
@@ -0,0 +1,13 @@
+var first = 66;
+var second = "Привет! Я строка!";
+var three = (first - 2);
+var four = (second - 2);
+var five = (first * 2) + 11;
+var six = (first / 2) + "j";
+
+console.log(first);
+console.log(second);
+console.log(three);
+console.log(four);
+console.log(five);
+console.log(six);
